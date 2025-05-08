6085a757001cf2e23e863ae2cb043b116f68c05c
Sang Yum <syum@amplifyfederal.com>
2025-05-08 08:31:48 -0700

    simple netlify function

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,4 +1,8 @@
-export const handler = async () => {
+export const handler = async (event, context) => {
+  const eventBody = JSON.parse(event.body)
+
+  console.log(eventBody)
+
   return {
     statusCode: 200,
     body: JSON.stringify({
